338bbfa7d43aca0f3e9f20ba9c77a60288bc34ff
Erik Roth <erikroth@rltechs.com>
2021-07-16 07:52:45 -0600

    Add directories with placeholder

---
+++ scripts/wpds-setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# ./scripts/wpds-setup.sh 
+# Copyright (c) 2019-2021 RL Technologies LLC (AGPL v3.0 or Later)
+# <codeteam@wpdevstack.com>  <codeteam@rltechs.com>
+
+# Verify Ubuntu and Version
+
+# Create Sudo User
