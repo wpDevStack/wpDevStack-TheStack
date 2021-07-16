2d3de03676f3eb37dae33c857f389b2b474abd9d
Erik Roth <erikroth@rltechs.com>
2021-07-16 08:07:35 -0600

    Create suadmin.sh

---
+++ scripts/suadmin.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# ./scripts/wpds-setup.sh 
+# Copyright (c) 2019-2021 RL Technologies LLC (AGPL v3.0 or Later)
+# <codeteam@wpdevstack.com>  <codeteam@rltechs.com>
+
+# This script adds the sudo user (admin) account for wpDevStack. This account
+# is not for use and can not log on ocally. Once the install is complete, the
+# account will remain, and it will handle system updates and other chores.
